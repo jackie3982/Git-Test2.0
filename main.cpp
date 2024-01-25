f503db424e8a32fc5aa6f2289ebc32a355650d88
Jacqueline <jaccbybe@ut.utm.edu>
2024-01-24 23:44:11 -0600

    Fourth commit: New feature called sum.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,18 @@ CSCI 325 Spring24
 
 using namespace std;
 
+int sum(int n);
+// takes the integer as a parameter, sums the numbers from 1 to n, and returns the sum.
+
 int main() {
   cout << "Hello World...\n";
   
   return 0;
+}
+int sum(int n){
+  int total = 0;
+  for( int i = 0; i < n; i++){
+    total += i;
+  }
+  return total;
 }
